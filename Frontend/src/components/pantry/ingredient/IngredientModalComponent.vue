e2075c77c1aadcf6a46f9d93a6f0bb72ca6ce1cf
Avamegyeri <90260185+Avamegyeri@users.noreply.github.com>
2023-03-08 08:37:48 -0500

    24 confirmation popup for delete (#26)
    * just adding the page for now
    
    * adding some things
    
    * finished the functionality
    
    * idk what i changed
    
    * css tries
    
    * adding transition
    
    * Changing some of the CSS
    
    * Fixing bug propagation event
    
    ---------
    
    Co-authored-by: Ava Megyeri <avamegyeri@Avas-MacBook-Air.local>
    Co-authored-by: MelvinAuvray <melvin.auvray@hotmail.fr>

--- Frontend/src/components/pantry/ingredient/IngredientModalComponent.vue
+++ Frontend/src/components/pantry/ingredient/IngredientModalComponent.vue
@@ -2,75 +2,72 @@
 import { IngredientCat } from "@/model/PantryModels";
 import IngredientListComponent from "@/components/pantry/ingredient/IngredientListComponent.vue";
 import CustomButton from "@/components/formComponents/CustomButtonComponent.vue";
- export default {
-    name: "IngredientModalComponent",
-    emits: ["close", "delete-ingredient", "add-ingredient"],
-    props: {
-        ingredientcat: {
-            type: IngredientCat,
-            required: true
-        },
-
+export default {
+  name: "IngredientModalComponent",
+  emits: ["close", "delete-ingredient", "add-ingredient"],
+  props: {
+    ingredientcat: {
+      type: IngredientCat,
+      required: true
     },
 
-    methods: {
-      close() {
-        this.$emit("close");
-      },
+  },
+
+  methods: {
+    close() {
+      this.$emit("close");
     },
-    components: {
-        IngredientListComponent,
-        CustomButton
-    }
-  };
+  },
+  components: {
+    IngredientListComponent,
+    CustomButton
+  }
+};
 </script>
 
 <template>
   <transition name="modal-fade">
     <div class="modal-overlay">
       <div class="modal" role="dialog">
-          <div class="header">
-            <h2>{{ ingredientcat.name }}</h2>
-            <div class="btn-close">
-              <CustomButton type="danger" effect="inline" text="" titleText="Close category" icon="x"
-              @clicked="close" />
+        <div class="header">
+          <h2>{{ ingredientcat.name }}</h2>
+          <div class="btn-close">
+            <CustomButton type="danger" effect="inline" text="" titleText="Close category" icon="x" @clicked="close" />
           </div>
         </div>
-        <IngredientListComponent
-          @delete-ingredient="(ingredient) => $emit('delete-ingredient', ingredient)"
-          @add-ingredient="(nameAdd) => $emit('add-ingredient', nameAdd)"
-          :ingredients="ingredientcat.ingredients" />          
-        </div>
+        <IngredientListComponent @delete-ingredient="(ingredient) => $emit('delete-ingredient', ingredient)"
+          @add-ingredient="(nameAdd) => $emit('add-ingredient', nameAdd)" :ingredients="ingredientcat.ingredients" />
+      </div>
     </div>
   </transition>
 </template>
 
 <style scoped>
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 2;
-  }
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+}
 
-  .modal {
-    width: 40%;
-    border-radius: 25px;
-    background: var(--color-background);
-    box-shadow: 2px 2px 20px 1px;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    position: relative;
-    z-index: 3;
-  }
+.modal {
+  width: 40%;
+  border-radius: 25px;
+  background: var(--color-background);
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  position: relative;
+  z-index: 3;
+}
 
 .header {
   flex-direction: row;
@@ -81,22 +78,21 @@ import CustomButton from "@/components/formComponents/CustomButtonComponent.vue"
   padding: 5px;
 }
 
-  .btn-close {
-    position: absolute;
-    top: 1px;
-    right: 0;
-    padding-top: 5px;
-    padding-right: 10px;
-  }
+.btn-close {
+  position: absolute;
+  top: 1px;
+  right: 0;
+  padding-top: 5px;
+  padding-right: 10px;
+}
 
-  .modal-fade-enter-active,
+.modal-fade-enter-active,
 .modal-fade-leave-active {
-    transition: all 0.5s ease;
+  transition: all 0.5s ease;
 }
 
 .modal-fade-enter-from,
 .modal-fade-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
-
 </style>

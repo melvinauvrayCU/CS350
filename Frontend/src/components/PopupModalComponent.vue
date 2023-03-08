4d5222b0b6b447bb489fef4cfa0786c430c0d05f
AvaMegyeri <megyeram@clarkson.edu>
2023-03-07 19:06:30 -0500

    adding transition

--- Frontend/src/components/PopupModalComponent.vue
+++ Frontend/src/components/PopupModalComponent.vue
@@ -3,65 +3,72 @@ import CustomButton from "@/components/formComponents/CustomButtonComponent.vue"
 
 
 export default {
-    name: "PopupModal",
-    components : {
-        CustomButton
-    },
+  name: "PopupModal",
+  components: {
+    CustomButton
+  },
 };
 
 </script>
 
 
 <template>
-    <div class = "modal-overlay">
-        <div class = "modal">
-            <div class = "header">
-                
-                <p> Are you sure you want to delete </p>
-                <div class = "btn-close">
-                    <CustomButton type="danger" effect="empty" text="x" titleText="Close" @click="$emit('close-modal')"/>
-                </div>
-                <div class = "btn-confirm">
-                    <CustomButton type="success" effect="empty" text="confirm" titleText="confirm" @clicked ="$emit('confirm-modal')"/>
+  <transition name="modal-fade">
+    <div class="modal-overlay">
+      <div class="modal">
+        <div class="header">
+          <h2>Are you sure you want to delete this recipe?</h2>
+          <div class="btn-close">
+            <CustomButton type="danger" effect="inline" text="" titleText="Close category" icon="x"
+              @clicked="$emit('close-modal')" />
+          </div>
+          <div class="btn-confirm">
+            <CustomButton type="success" effect="empty" text="confirm" titleText="confirm"
+              @clicked="$emit('confirm-modal')" />
 
-                </div>
-                <div class = "btn-cancel">
-                    <CustomButton type="neutral" effect="empty" text="cancel" titleText="cancel" @clicked ="$emit('cancel-modal')"/>
+          </div>
+          <div class="btn-cancel">
+            <CustomButton type="neutral" effect="empty" text="cancel" titleText="cancel"
+              @clicked="$emit('cancel-modal')" />
 
-                </div>
-
-            </div>
+          </div>
 
         </div>
 
+      </div>
+
     </div>
+  </transition>
 </template>
 
 <style scoped>
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 2;
-  }
-  .modal {
-    width: 40%;
-    border-radius: 25px;
-    background: var(--color-background);
-    box-shadow: 2px 2px 20px 1px;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    position: relative;
-    z-index: 3;
-  }
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+}
+
+.modal {
+  width: 30%;
+  height: 15%;
+  border-radius: 25px;
+  background: var(--color-background);
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  position: relative;
+  z-index: 3;
+}
+
 .header {
   flex-direction: row;
   justify-content: space-between;
@@ -70,16 +77,22 @@ export default {
   align-items: baseline;
   padding: 5px;
 }
-  .btn-close {
-    position: absolute;
-    top: 1px;
-    right: 0;
-    padding-top: 5px;
-    padding-right: 10px;
-  }
+
+.header h2 {
+  text-align: center;
+
+}
+
+.btn-close {
+  position: absolute;
+  top: 1px;
+  right: 0;
+  padding-top: 5px;
+  padding-right: 10px;
+}
 
 
-  .btn-cancel {
+.btn-cancel {
   margin-top: 10px;
   width: 50%;
   float: left;
@@ -88,9 +101,16 @@ export default {
 .btn-confirm {
   margin-top: 10px;
   width: 50%;
-  float: right;
+  float: left;
 }
 
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: all 0.5s ease;
+}
 
-
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+  opacity: 0;
+}
 </style>

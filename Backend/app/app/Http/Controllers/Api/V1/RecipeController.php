3bd87616d92c2ddb7a054ba781bcdd4ce8651433
MelvinAuvray <melvin.auvray@hotmail.fr>
2023-04-24 21:30:23 -0400

    Goodbye RecipeBuddy

--- Backend/app/app/Http/Controllers/Api/V1/RecipeController.php
+++ Backend/app/app/Http/Controllers/Api/V1/RecipeController.php
@@ -39,7 +39,7 @@ class RecipeController extends Controller
             $recipes = $recipes->with('user');
         }
 
-        return new RecipeCollection($recipes);
+        return new RecipeCollection($recipes->get());
     }
 
     /**
